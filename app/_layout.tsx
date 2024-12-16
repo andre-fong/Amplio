6375718437a15f8462c88306ed8bcb1a5388bc29
Andre Fong <fongandre123@gmail.com>
2024-12-15 20:28:45 -0500

    chore: Enforce dark theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, MD3DarkTheme } from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <>
-      <PaperProvider>
+      <PaperProvider theme={MD3DarkTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
